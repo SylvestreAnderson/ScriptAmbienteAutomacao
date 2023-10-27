bd66bbb230f449b9f068ae64277633cc7feddeaf
SylvestreAnderson <sylvestre.anderson@gmail.com>
2023-10-27 10:04:47 -0300

    Corrigindo o script

--- install_current_automation.ps1
+++ install_current_automation.ps1
@@ -80,7 +80,7 @@ function InstandoJava {
 }
 
 function instalandoAndroidStudio {
-    if(Test-Path "C:\Users\$usuario\AppData\Local\Android"){
+    if(Test-Path "C:\Program Files\Android"){
         Write-Output "Android Studio ja esta instalado!"
     } else {
         Set-Location "C:\programasAutomacao"
@@ -91,8 +91,7 @@ function instalandoAndroidStudio {
         }
 
         if(Test-Path "C:\programasAutomacao\AndroidStudio.exe"){
-            Write-Output "Instalando o Android Studio na maquina Aguarde.."
-            Start-Process "C:\programasAutomacao\AndroidStudio.exe" -ArgumentList "/quiet"
+            Write-Output "Instalando o Android Studio na maquina Aguarde..."
             $Install = Start-Process "C:\programasAutomacao\AndroidStudio.exe " -ArgumentList "/S" -PassThru
             while ($Install.ExitCode -eq $null){Sleep 1} 
             Write-Output "Instalacao do Android Studio concluido!"
@@ -105,9 +104,9 @@ function instalandoAndroidStudio {
 
 function setVariaviesSistema  {
     Write-Output "Conigurando as variaveis de ambiente sistema ..."
-    [Environment]::SetEnvironmentVariable('JAVA_HOME', 'C:\Program Files\OpenLogic\jdk-8.0.392.08-hotspot\bin', 'Machine')
-    [Environment]::SetEnvironmentVariable('ANDROID_HOME', 'C:\Users\$usuario\AppData\Local\Android\Sdk', 'Machine')  
-    [Environment]::SetEnvironmentVariable('PAth', '%SystemRoot%\system32' + ';%SystemRoot%' + ';%SystemRoot%\System32\Wbem' + ';%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\' + ';%SYSTEMROOT%\System32\OpenSSH\' + 'C:\Program Files\Git\cmd' + 'C:\Program Files\nodejs\', 'Machine')  
+    [Environment]::SetEnvironmentVariable('JAVA_HOME', 'C:\Program Files\OpenLogic\jdk-8.0.392.08-hotspot\', 'Machine')
+    [Environment]::SetEnvironmentVariable("ANDROID_HOME", "C:\Users\" + $usuario + "\AppData\Local\Android\Sdk", "Machine")  
+    [Environment]::SetEnvironmentVariable('Path', '%SystemRoot%\system32' + ';%SystemRoot%' + ';%SystemRoot%\System32\Wbem' + ';%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\' + ';%SYSTEMROOT%\System32\OpenSSH\' + ';C:\Program Files\Git\cmd' + ';C:\Program Files\nodejs\', 'Machine')  
         
 
     Write-Output "Conigurando as variaveis de ambiente usuario ..."
@@ -117,93 +116,17 @@ function setVariaviesSistema  {
 # instalando Appium
 function instalandoAppium {
     Write-Output "Instalando o Appium na versão 1.22.3"
-    ni -n installAppium.bat . -v 'npm install -g appium@1.22.3 \n'
-    ni -n installAppium.bat . -v 'nppip install -r requirements.txt'
+    tart-Process npm install -g appium@1.22.3 
+    tart-Process pip install -r requirements.txt
 }
 
-#========================================================================================================================================================================================================================
 
-<<<<<<< HEAD
-function installChocolaty{
-    if(-not (Test-Path "C:\ProgramData\chocolatey")){
-        Write-Output "Chocolaty ja instalado na maquina!"
-    } else {
-        Write_Output "Instalando o Chocolaty Aguarde..."
-        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-    }
-}
-
-function installPython {
-    if(-not (Test-Path "C:\ProgramData\chocolatey\.chocolatey\python.3.12.0")){
-        Write-Output "O Python ja esta instalado na maquina!"
-    } else {
-        Write-Output "Instalando o Python na maquina Aguarde..."    
-        choco install python -y
-    }
-    
-}
-
-function installNodeJs {
-    if(-not (Test-Path "C:\ProgramData\chocolatey\.chocolatey\nodejs.21.1.0")){
-        Write-Output "O Nodejs ja esta instalado na maquina!"
-    } else {
-        Write-Output "Instalando o NodeJs na maquina Aguarde..."    
-        choco install choco install nodejs -y
-    }
-    
-}
-
-function installJava8 {
-    if(-not (Test-Path "C:\ProgramData\chocolatey\.chocolatey\ojdkbuild8.8.0.332.20220914")){
-        Write-Output "O Java 8 ja esta instalado na maquina!"
-    } else {
-        Write-Output "Instalando o Java 8 na maquina Aguarde..."    
-        choco install ojdkbuild8 -y
-    }
-    
-}
-
-function installAndroidStudio {
-    if(-not (Test-Path "C:\ProgramData\chocolatey\.chocolatey\androidstudio.2022.3.1.20")){
-        Write-Output "O Android Studio ja esta instalado na maquina!"
-    } else {
-        Write-Output "Instalando o Android Studio na maquina Aguarde..."    
-        choco install androidstudio -y
-    }
-    
-}
-
-function installGit {
-    if(-not (Test-Path "C:\ProgramData\chocolatey\.chocolatey\git.2.42.0")){
-        Write-Output "O Git ja esta instalado na maquina!"
-    } else {
-        Write-Output "Instalando o Android Studio na maquina Aguarde..."    
-        choco install git -y
-    }
-    
-}
-
-function installVscode {
-    if(-not (Test-Path "C:\ProgramData\chocolatey\.chocolatey\vscode.1.83.1")){
-        Write-Output "O Vscode ja esta instalado na maquina!"
-    } else {
-        Write-Output "Instalando o Vscode na maquina Aguarde..."    
-        choco install vscode -y
-    }
-}
-
-installChocolaty
-installPython
-installNodeJs
-installJava8
-installAndroidStudio
-installVscode
 
 #=========================================================================
-#CriandoPastaInstalacao
-#InstalandoPython
-#IntalarNodeJs
-#InstandoJava
-#instalandoAndroidStudio
-#instalandoAppium
-setVariaviesSistema
+CriandoPastaInstalacao
+InstalandoPython
+IntalarNodeJs
+InstandoJava
+instalandoAndroidStudio
+instalandoAppium
+#setVariaviesSistema
